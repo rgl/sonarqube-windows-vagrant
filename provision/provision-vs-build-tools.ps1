4afa5ca3e5dca6c32da406f0ec71187899ca0782
Rui Lopes <rgl@ruilopes.com>
2019-05-22 22:35:56 +0100

    upgrade to vs2019 build tools

--- provision/provision-vs-build-tools.ps1
+++ provision/provision-vs-build-tools.ps1
@@ -5,23 +5,23 @@ choco install -y netfx-4.5.2-devpack
 
 # install the Visual Studio Build Tools.
 # see https://www.visualstudio.com/downloads/
-# see https://www.visualstudio.com/en-us/news/releasenotes/vs2017-relnotes
-# see https://docs.microsoft.com/en-us/visualstudio/install/use-command-line-parameters-to-install-visual-studio
-# see https://docs.microsoft.com/en-us/visualstudio/install/command-line-parameter-examples
-# see https://docs.microsoft.com/en-us/visualstudio/install/workload-and-component-ids
-# see https://docs.microsoft.com/en-us/visualstudio/install/workload-component-id-vs-build-tools
-$archiveUrl = 'https://download.visualstudio.microsoft.com/download/pr/11669063/e64d79b40219aea618ce2fe10ebd5f0d/vs_BuildTools.exe'
-$archiveHash = 'b418db13c66a44b0d13dc38c3fdb07f724fce19dbfad2e29f08bf87239fc3bed'
+# see https://docs.microsoft.com/en-us/visualstudio/releases/2019/release-notes
+# see https://docs.microsoft.com/en-us/visualstudio/install/use-command-line-parameters-to-install-visual-studio?view=vs-2019
+# see https://docs.microsoft.com/en-us/visualstudio/install/command-line-parameter-examples?view=vs-2019
+# see https://docs.microsoft.com/en-us/visualstudio/install/workload-and-component-ids?view=vs-2019
+# see https://docs.microsoft.com/en-us/visualstudio/install/workload-component-id-vs-build-tools?view=vs-2019
+$archiveUrl = 'https://download.visualstudio.microsoft.com/download/pr/a08183e4-3087-4df5-a074-d3bdf1ad5eb8/20816d670f7909277d9793dc3e80b3c2/vs_buildtools.exe'
+$archiveHash = '23664c1f1d7cd86f6804c4840221a60952a9f2ef6540eb195d6da732f0e703e0'
 $archiveName = Split-Path $archiveUrl -Leaf
 $archivePath = "$env:TEMP\$archiveName"
 Write-Host 'Downloading the Visual Studio Build Tools Setup Bootstrapper...'
-Invoke-WebRequest $archiveUrl -UseBasicParsing -OutFile $archivePath
+(New-Object Net.WebClient).DownloadFile($archiveUrl, $archivePath)
 $archiveActualHash = (Get-FileHash $archivePath -Algorithm SHA256).Hash
 if ($archiveHash -ne $archiveActualHash) {
     throw "$archiveName downloaded from $archiveUrl to $archivePath has $archiveActualHash hash witch does not match the expected $archiveHash"
 }
 Write-Host 'Installing the Visual Studio Build Tools...'
-$vsBuildToolsHome = 'C:\VS2017BuildTools'
+$vsBuildToolsHome = 'C:\VS2019BuildTools'
 for ($try = 1; ; ++$try) {
     &$archivePath `
         --installPath $vsBuildToolsHome `
@@ -45,5 +45,12 @@ for ($try = 1; ; ++$try) {
 # add MSBuild to the machine PATH.
 [Environment]::SetEnvironmentVariable(
     'PATH',
-    "$([Environment]::GetEnvironmentVariable('PATH', 'Machine'));$vsBuildToolsHome\MSBuild\15.0\Bin",
+    "$([Environment]::GetEnvironmentVariable('PATH', 'Machine'));$vsBuildToolsHome\MSBuild\Current\Bin",
+    'Machine')
+
+# prevent msbuild from running in background, as that will interfere with
+# cleaning the job workspace due to open files/directories.
+[Environment]::SetEnvironmentVariable(
+    'MSBUILDDISABLENODEREUSE',
+    '1',
     'Machine')
